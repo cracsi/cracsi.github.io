50295861218d97aa958d23b1a753a6f7ed15677e
cracsi <98497136+cracsi@users.noreply.github.com>
2024-08-28 21:20:04 -0500

    final prueba

--- popup.js
+++ popup.js
@@ -2,19 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const calcularBtn = document.getElementById('calcularBtn');
     const popupForm = document.getElementById('popupForm');
     const closePopup = document.getElementById('closePopup');
+    const overlay = document.getElementById('overlay');
 
     calcularBtn.addEventListener('click', function () {
+        overlay.style.display = 'block';
         popupForm.style.display = 'block';
     });
 
     closePopup.addEventListener('click', function () {
+        overlay.style.display = 'none';
         popupForm.style.display = 'none';
     });
 
     // Cierra el pop-up si se hace clic fuera de él
-    window.addEventListener('click', function (event) {
-        if (event.target == popupForm) {
-            popupForm.style.display = 'none';
-        }
+    overlay.addEventListener('click', function () {
+        overlay.style.display = 'none';
+        popupForm.style.display = 'none';
     });
 });
